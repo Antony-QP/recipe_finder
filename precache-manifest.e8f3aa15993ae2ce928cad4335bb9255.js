31e84d27190fc052287e19f412a091b3e93b9b52
Antony Parker <antonyqparker@gmail.com>
2021-02-06 16:19:57 +0100

    Updates

---
+++ precache-manifest.e8f3aa15993ae2ce928cad4335bb9255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f50bb2ae8be39894dff52faf7d32f83",
+    "url": "/recipe_finder/index.html"
+  },
+  {
+    "revision": "e99b88355517cb09812f",
+    "url": "/recipe_finder/static/css/main.6cb9eaca.chunk.css"
+  },
+  {
+    "revision": "cea2ff2c95c1a43d8e5c",
+    "url": "/recipe_finder/static/js/2.1d616969.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe_finder/static/js/2.1d616969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99b88355517cb09812f",
+    "url": "/recipe_finder/static/js/main.51600568.chunk.js"
+  },
+  {
+    "revision": "52fb3a6b6129a2da328d",
+    "url": "/recipe_finder/static/js/runtime-main.c8294712.js"
+  }
+]);
